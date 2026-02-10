6d0f56759b979f14e7aa364cfa36f322a9d46936
Rafael Toledo <toledorafael304@gmail.com>
2026-02-10 15:51:15 -0300

    fix/doc: fix documentation of user controller

--- userservice/src/main/java/dev/rt/fitness/userservice/controllers/UserController.java
+++ userservice/src/main/java/dev/rt/fitness/userservice/controllers/UserController.java
@@ -8,6 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * REST controller responsible fo user-related endpoints.
+ *
+ * <p>Handles HTTP requests and delegates business logic to the service layer.</p>
+ */
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -18,11 +23,25 @@ public class UserController {
     this.userService = userService;
   }
 
+  /**
+   * Retrieve a User by ID.
+   *
+   * @param userId the user's ID
+   *
+   * @return the user's profile data as a {#{@link UserResponse}}
+   */
   @GetMapping("/{userId}")
   public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId) {
     return ResponseEntity.ok(userService.getUserProfile(userId));
   }
 
+  /**
+   * Registers a new user.
+   *
+   * @param request validated user registration data
+   *
+   * @return the created user response
+   */
   @PostMapping("/register")
   public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody UserRegisterRequest request) {
     return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(request));
